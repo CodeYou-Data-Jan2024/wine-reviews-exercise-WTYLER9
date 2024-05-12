164d65b90556fb86726cef46a31cdb3514be8de9
WTYLER9 <wtyler9889@gmail.com>
2024-05-11 23:27:38 -0400

    Wine Reviews Complete

--- reviews.py
+++ reviews.py
@@ -1,2 +1,9 @@
-# add your code here
+import pandas as pd
 
+reviews = pd.read_csv("winemag-data-130k-v2.csv.zip", compression = "zip", index_col=0)
+
+reviews_per_country = pd.DataFrame(reviews.country.value_counts())
+avgpts = pd.DataFrame(reviews.groupby("country").points.mean().round(1))
+
+combined = pd.concat([reviews_per_country,avgpts],axis=1,join="inner")
+combined.to_csv("reviews-per-country.csv",encoding="utf-8")
